f9ecc7a49217cf2ffcd3b01327c9d3f19d6b596e
ChandrakantPal <sanjay161097@gmail.com>
2021-06-05 01:45:56 +0530

    subtask resolvers

---
+++ graphql/resolvers/index.js
@@ -0,0 +1,17 @@
+const tasksResolvers = require('./tasks')
+const usersResolvers = require('./users')
+const subTasksResolvers = require('./subTasks')
+
+module.exports = {
+  Query: {
+    ...tasksResolvers.Query,
+  },
+  Mutation: {
+    ...usersResolvers.Mutation,
+    ...tasksResolvers.Mutation,
+    ...subTasksResolvers.Mutation,
+  },
+  Subscription: {
+    ...tasksResolvers.Subscription,
+  },
+}
