6a723cebea7f01a73499910534294a50ee400e7f
ChandrakantPal <sanjay161097@gmail.com>
2021-06-05 01:46:12 +0530

    sub task resolvers issue

---
+++ graphql/resolvers/subTasks.js
@@ -0,0 +1,60 @@
+const { UserInputError, AuthenticationError } = require('apollo-server')
+
+const Task = require('../../models/Task')
+const checkAuth = require('../../utils/checkAuth')
+
+module.exports = {
+  Mutation: {
+    createSubTask: async (_, { taskId, body }, context) => {
+      const { username } = checkAuth(context)
+      if (body.trim() === '') {
+        throw new UserInputError('Empty comment', {
+          errors: {
+            body: 'Comment body must not be empty',
+          },
+        })
+      }
+      const newSubTask = new Task({
+        body,
+        user: context.user.id,
+        username: username,
+        createdAt: new Date().toISOString(),
+        subTasks: [],
+      })
+      const subTask = await newSubTask.save()
+      const task = await Task.findById(taskId)
+      console.log({ task, subTask })
+
+      if (task) {
+        task.subTasks.unshift({
+          id: subTask.id,
+        })
+        await task.save()
+        return task
+      } else {
+        throw new UserInputError('Post not founnd')
+      }
+    },
+    deleteSubTask: async (_, { taskId, subTaskId }, context) => {
+      const { username } = checkAuth(context)
+
+      const task = await Task.findById(taskId)
+
+      if (task) {
+        const subTaskIndex = task.subTasks.findIndex(
+          (subTask) => subTask.id === subTaskId
+        )
+
+        if (task.subTasks[subTaskIndex].username === username) {
+          task.subTasks.splice(subTaskIndex, 1)
+          await task.save()
+          return task
+        } else {
+          throw new AuthenticationError('Action not allowed')
+        }
+      } else {
+        throw new UserInputError('Post not founnd')
+      }
+    },
+  },
+}
