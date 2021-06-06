4c82c6907b15cd71c6ff3779ac8211a651b1b7a5
ChandrakantPal <sanjay161097@gmail.com>
2021-06-07 01:22:38 +0530

    login moved to query

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,13 +1,13 @@
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const { UserInputError } = require("apollo-server");
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const { UserInputError } = require('apollo-server')
 
 const {
   validateRegisterInput,
   validateLoginInput,
-} = require("../../utils/validators");
-const { SECRET_KEY } = require("../../config");
-const User = require("../../models/User");
+} = require('../../utils/validators')
+const { SECRET_KEY } = require('../../config')
+const User = require('../../models/User')
 
 const generateToken = (user) => {
   return jwt.sign(
@@ -17,39 +17,41 @@ const generateToken = (user) => {
       username: user.username,
     },
     SECRET_KEY,
-    { expiresIn: "1h" }
-  );
-};
+    { expiresIn: '1h' }
+  )
+}
 
 module.exports = {
-  Mutation: {
+  Query: {
     async login(_, { username, password }) {
-      const { errors, valid } = validateLoginInput(username, password);
+      const { errors, valid } = validateLoginInput(username, password)
 
       if (!valid) {
-        throw new UserInputError("Errors", { errors });
+        throw new UserInputError('Errors', { errors })
       }
 
-      const user = await User.findOne({ username });
+      const user = await User.findOne({ username })
 
       if (!user) {
-        errors.general = "User not found";
-        throw new UserInputError("User not found", { errors });
+        errors.username = 'User not found'
+        throw new UserInputError('User not found', { errors })
       }
 
-      const match = await bcrypt.compare(password, user.password);
+      const match = await bcrypt.compare(password, user.password)
       if (!match) {
-        errors.general = "Wrong credetials";
-        throw new UserInputError("Wrong credetials", { errors });
+        errors.password = 'Wrong credetials'
+        throw new UserInputError('Wrong credetials', { errors })
       }
 
-      const token = generateToken(user);
+      const token = generateToken(user)
       return {
         ...user._doc,
         id: user._id,
         token,
-      };
+      }
     },
+  },
+  Mutation: {
     async register(
       _,
       { registerInput: { username, email, password, confirmPassword } }
@@ -59,38 +61,38 @@ module.exports = {
         email,
         password,
         confirmPassword
-      );
+      )
       if (!valid) {
-        throw new UserInputError("Errors", { errors });
+        throw new UserInputError('Errors', { errors })
       }
 
-      const user = await User.findOne({ username });
+      const user = await User.findOne({ username })
       if (user) {
-        throw new UserInputError("Username is taken", {
+        throw new UserInputError('Username is taken', {
           errors: {
-            username: "This username is taken",
+            username: 'This username is taken',
           },
-        });
+        })
       }
 
       // hash password and creating token
-      password = await bcrypt.hash(password, 12);
+      password = await bcrypt.hash(password, 12)
 
       const newUser = new User({
         email,
         username,
         password,
         createdAt: new Date().toISOString(),
-      });
-      const res = await newUser.save();
+      })
+      const res = await newUser.save()
 
-      const token = generateToken(res);
+      const token = generateToken(res)
 
       return {
         ...res._doc,
         id: res._id,
         token,
-      };
+      }
     },
   },
-};
+}
